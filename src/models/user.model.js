f2dc854e0e2c06ed87d9ad8d1adc7eba65eea601
pranshu6sept <128319537+pranshu6sept@users.noreply.github.com>
2024-02-07 18:11:09 +0530

    login and logout done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -43,9 +43,9 @@ const userSchema = new Schema(
             type:String,
             required:[true,'Password is required']
         },
-        referToken:{
-            type:String
-        }
+        refreshToken: {
+            type: String,
+          },
 
 },
 {
@@ -62,27 +62,30 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAcessToken = function(){
-    jwt.sign({
-        _id : this._id,
-        email: this.email,
-        userrname: this.userrname,
-        fullname: this.fullname
-    },
-    process.env.ACCESS_TOKEN_SECERT,
-    {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-    }
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
     )
 }
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign({
-        _id : this._id,
-    },
-    process.env.REFERSH_TOKEN_SECERT,
-    {
-        expiresIn: process.env.REFERSH_TOKEN_EXPIRY
-    }
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
     )
 }
 
